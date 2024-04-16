93203a3dd08c221a282af6d5ce05384151b10afc
Anton Dyuzhev <prklphln@gmail.com>
2024-04-16 17:50:03 +0300

    Add some changes to Horse tests

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -4,14 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
     @Test
-    public void HorseConstructorIllegalArgumentException() {
+    public void HorseConstructorNullException() {
         assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10.0, 100.0));
     }
-@Test
-public void HorseConstructorNameNotNullExpression() {
-        String exceptionString = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10.0, 100.0)).getMessage();
-assertEquals(exceptionString, "Name cannot be null.");
-}
+
+    @Test
+    public void HorseConstructorNullMessageExpression() {
+        String exceptionString = assertThrows(IllegalArgumentException.class,
+                () -> new Horse(null, 10.0, 100.0)).getMessage();
+        assertEquals(exceptionString, "Name cannot be null.");
+    }
+
+    @Test
+    public void HorseConstructorSpaceSymbolsException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Horse("\s", 10.0, 100.0));
+    }
+
+    @Test
+    public void HorseConstructorSpaceSymbolsMessage() {
+        String exceptionString = assertThrows(IllegalArgumentException.class,
+                () -> new Horse("\s", 10.0, 100.0)).getMessage();
+        assertEquals(exceptionString, "Name cannot be blank.");
+    }
 
     @Test
     void getName() {
