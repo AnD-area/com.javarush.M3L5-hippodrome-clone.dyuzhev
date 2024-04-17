2d6f19753dc04fe63bafbbbd858fbff8fe3b8bb0
Anton Dyuzhev <prklphln@gmail.com>
2024-04-17 17:42:26 +0300

    End of HippodromeTest + MainTest

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 public class HippodromeTest {
 
@@ -39,18 +39,25 @@ public class HippodromeTest {
     public void move() {
         List<Horse> horses = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
-            horses.add(mock(Horse.class));
+            horses.add(Mockito.mock(Horse.class));
         }
         Hippodrome hippo = new Hippodrome(horses);
 
         hippo.move();
 
-        for (Horse key : horses) {
-            Mockito.verify(key).move();
+        for (Horse oneHorse : horses) {
+            Mockito.verify(oneHorse).move();
         }
     }
 
     @Test
     public void getWinner() {
+        List<Horse> horses = new ArrayList<>();
+        Horse horse1 = new Horse("name1", 5, 10);
+        Horse horse2 = new Horse("name2", 5, 11);
+        Horse horse3 = new Horse("name3", 5, 8);
+        Collections.addAll(horses, horse1, horse2, horse3);
+        Hippodrome hippo = new Hippodrome(horses);
+        assertSame(horse2, hippo.getWinner());
     }
 }
